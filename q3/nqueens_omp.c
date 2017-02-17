04b8a861b74b100305b7f3c836ef5f86aebb270d
Ghanan Gowripalan <ghanan94@gmail.com>
2017-02-17 12:31:06 -0500

    1.3x speedup with openmp directives

--- q3/nqueens_omp.c
+++ q3/nqueens_omp.c
@@ -3,15 +3,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define TASK_IF_I_LESS_THAN_VAL (4)
+
 int safe(char * config, int i, int j)
 {
     int r, s;
 
     for (r = 0; r < i; r++)
     {
-        s = config[r];
-        if (j == s || i-r==j-s || i-r==s-j)
-            return 0;
+	s = config[r];
+	if (j == s || i-r==j-s || i-r==s-j)
+	    return 0;
     }
     return 1;
 }
@@ -25,23 +27,32 @@ void nqueens(char *config, int n, int i)
 
     if (i==n)
     {
-        count++;
+	//
+	// Make sure increment of count is atomic (avoid race conditions)
+	//
+	#pragma omp atomic
+	count++;
     }
-    
+
     /* try each possible position for queen <i> */
     for (j=0; j<n; j++)
     {
-        /* allocate a temporary array and copy the config into it */
-        new_config = malloc((i+1)*sizeof(char));
-        memcpy(new_config, config, i*sizeof(char));
-        if (safe(new_config, i, j))
-        {
-            new_config[i] = j;
-	    nqueens(new_config, n, i+1);
-        }
-        free(new_config);
+	/* allocate a temporary array and copy the config into it */
+	#pragma omp task private(new_config) shared(config, n, i) firstprivate(j) if(i < TASK_IF_I_LESS_THAN_VAL)
+	{
+	    new_config = malloc((i+1)*sizeof(char));
+	    memcpy(new_config, config, i*sizeof(char));
+	    if (safe(new_config, i, j))
+	    {
+		new_config[i] = j;
+		nqueens(new_config, n, i+1);
+	    }
+	    free(new_config);
+	}
     }
+
     // sync
+    #pragma omp taskwait
     return;
 }
 
@@ -52,15 +63,29 @@ int main(int argc, char *argv[])
 
     if (argc < 2)
     {
-        printf("%s: number of queens required\n", argv[0]);
-        return 1;
+	printf("%s: number of queens required\n", argv[0]);
+	return 1;
     }
 
     n = atoi(argv[1]);
     config = malloc(n * sizeof(char));
 
     printf("running queens %d\n", n);
-    nqueens(config, n, 0);
+
+    //
+    // Create a section that will occur in parallel
+    //
+    #pragma omp parallel shared(config, n)
+    {
+	//
+	// Following block will only be executed by one thread
+	//
+	#pragma omp single
+	{
+	    nqueens(config, n, 0);
+	}
+    }
+
     printf("# solutions: %d\n", count);
 
     return 0;
